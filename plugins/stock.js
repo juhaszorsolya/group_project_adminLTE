cc2c051d5fe0506b27efbc5db8b90f2a4bf54c4a
Téglás Gábor <gaborteglas@gmail.com>
2018-06-30 07:57:27 +0200

    Adding stock.js

---
+++ plugins/stock.js
@@ -0,0 +1,57 @@
+var stock = [
+    {
+        id: 001,
+        name: 'iPhone X',
+        category: 'Smartphone',
+        price: 379990,
+        count: 7,
+        sold: 8,
+        createdAt: '2018-05-01',
+        updatedAt: '2018-06-20',
+    },
+
+    {
+        id: 002,
+        name: 'iPhone 8',
+        category: 'Smartphone',
+        price: 264990,
+        count: 11,
+        sold: 6,
+        createdAt: '2018-04-22',
+        updatedAt: '2018-06-25',
+    },
+
+    {
+        id: 003,
+        name: 'iPhone 7',
+        category: 'Smartphone',
+        price: 207990,
+        count: 14,
+        sold: 18,
+        createdAt: '2018-01-11',
+        updatedAt: '2018-04-17',
+    },
+
+    {
+        id: 004,
+        name: 'iPhone 6S',
+        category: 'Smartphone',
+        price: 168990,
+        count: 5,
+        sold: 14,
+        createdAt: '2017-10-12',
+        updatedAt: '2018-05-03',
+    },
+
+    {
+        id: 005,
+        name: 'iPhone SE',
+        category: 'Smartphone',
+        price: 129990,
+        count: 7,
+        sold: 32,
+        createdAt: '2017-12-05',
+        updatedAt: '2018-02-28',
+    },
+
+]
