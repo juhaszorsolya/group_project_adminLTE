f370e81aae793a69a6a684c722374f647969f512
Téglás Gábor <gaborteglas@gmail.com>
2018-06-30 14:31:38 +0200

    stock.js changes

--- gaborteglas.js
+++ gaborteglas.js
@@ -1,3 +1,13 @@
+/*1.feladat*/
+
+/*2.feladat*/
+var i = 1;
+var prices = [];
+while (i < stock.length) {
+    prices.push(stock[i].price);
+    i++;
+}
+console.log(Math.max(...prices))
 
 /*3. feladat*/
 var i = 1;
