fb791426c5e8c2f7659b6593a0ab4c58a02b53f3
Dóra Birgés <birgesdora@gmail.com>
2018-07-01 20:37:56 +0200

    add functions to dorabirges.js

--- dorabirges.js
+++ dorabirges.js
@@ -22,8 +22,8 @@ alert(newest.name);
 
 var sum = 0;
 for (var i = 0; i < stock.length; i++) {
-    sum += stock[i].price;
-    }
+    sum += stock[i].price * stock[i].count;
+}
 alert(sum);
 
 //Írja ki a termékek átlagárát
@@ -31,11 +31,63 @@ alert(sum);
 var sum = 0;
 for (var i = 0; i < stock.length; i++) {
     sum += stock[i].price;
-    }
+}
 alert(`${sum/stock.length}`);
 
 //Készítsetek egy függvényt, amely rendezi a termékeket ár szerint növekvő sorrendbe. Írjátok ki konzolra.
 
-function ascendingOrder() {
-    
+function ascendingOrder(stock) {
+    for (var i = 0; i < stock.length-1; i++) {
+        var smallest = i;
+        for (var j = i+1; j < stock.length; j++) {
+            if (stock[smallest].price > stock[j].price) {
+                smallest = j;
+            }
+        }
+        var change = stock[i];
+        stock[i] = stock[smallest];
+        stock[smallest] = change;
+    }
 }
+ascendingOrder(stock);
+
+//Készítsetek egy függvényt, ami leszűri a 3 legnépszerűbb terméket, azaz amelyekből a legtöbbet adtak el. Írjátok ki konzolra.
+
+function threeMostPopular(stock) {
+
+    for (var i = 0; i < 3; i++) {
+        var popular = i;
+        for (var j = i+1; j < stock.length; j++) {
+            if (stock[popular].sold < stock[j].sold) {
+                popular = j;
+            }
+        }
+        var change = stock[i];
+        stock[i] = stock[popular];
+        stock[popular] = change;
+    }
+    console.log(`A három legnépszerűbb termék:\n${stock[0].name} \n${stock[1].name} \n${stock[2].name}`)
+}
+
+
+threeMostPopular(stock);
+
+//Készítsetek egy függvényt, ami leszűri a 3 legújabb terméket. Írjátok ki konzolra.
+
+function threeNewestProduct(stock) {
+
+    for (var i = 0; i < 3; i++) {
+        var newest = i;
+        for (var j = i+1; j < stock.length; j++) {
+            if (stock[newest].createdAt < stock[j].createdAt) {
+                newest = j;
+            }
+        }
+        var change = stock[i];
+        stock[i] = stock[newest];
+        stock[newest] = change;
+    }
+    console.log(`A három legújabb termék:\n${stock[0].name} \n${stock[1].name} \n${stock[2].name}`)
+}
+
+threeNewestProduct(stock);
